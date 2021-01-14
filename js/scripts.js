7a149c971cb9c6a1edb1a076eae39bb3b8850ef4
dcouch440 <dcouch440@gmail.com>
2021-01-14 13:13:28 -0800

    add functionality to switching picutres using Jquery conditions
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  
-})
+  $("#submit-button").click(function(event) {
+    event.preventDefault()
+    const buttonValue = $("input:radio[name=exampleRadios]:checked").val();
+    console.log(buttonValue)
+    if (buttonValue === "option1") {
+      $("#bearImg").hide()
+      $("#tigerImg").hide()
+      $("#lionImg").show()
+    } else if (buttonValue === "option2") {
+      $("#bearImg").hide()
+      $("#lionImg").hide()
+      $("#tigerImg").show()
+    } else if (buttonValue === "option3") {
+      $("#tigerImg").hide()
+      $("#lionImg").hide()
+      $("#bearImg").show()
+    }
+  })
+});
